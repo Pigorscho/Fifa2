3e3e65b10b3b0794529ffcaee76d026346fb64b3
Pigorscho <lennartpigorsch@gmail.com>
2023-10-03 22:21:42 +0200

    latest

---
+++ kill.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.system('adb -s emulator-5554 shell reboot -p')
---
+++ scripts/utils/clean_up.py
@@ -0,0 +1,24 @@
+import os
+
+from settings import settings
+
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        print(f"File {file_path} has been deleted.")
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except PermissionError:
+        print(f"Permission denied. Couldn't delete {file_path}.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def clean_up():
+    for key, val in settings.items():
+        delete_file(rf"pics\screen-{val['port']}.png")
+
+
+def clean_single(port):
+    delete_file(rf'pics\screen-{port}.png')
